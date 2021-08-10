e1730868beac52e559a3d1c2ecb6d74d414a495d
Martin McKenna <33032026+MartinMcK@users.noreply.github.com>
2021-08-10 15:04:38 +0100

    updates

---
+++ COMP3000 Riff Racer/Assets/Code/UIAppear.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppear : MonoBehaviour
+{
+    [SerializeField]
+    private Image customImage;
+
+    void onTriggerEnter (Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            customImage.enabled = true;
+
+        }
+    }
+    void onTriggerExit(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            customImage.enabled = false;
+
+        }
+    }
+}
